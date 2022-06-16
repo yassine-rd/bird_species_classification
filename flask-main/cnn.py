a08839edfe54374ed9687a727129f2cace2af010
Yassine RODANI <76453252+yassine-rd@users.noreply.github.com>
2022-06-16 08:47:51 +0200

    updating project

--- flask-main/cnn.py
+++ flask-main/cnn.py
@@ -1,23 +1,30 @@
+import os
 import pathlib
 import pandas as pd
-import matplotlib.pyplot as plt
-from tensorflow import keras
+
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, MaxPooling2D, Flatten, Conv2D, Dropout, BatchNormalization
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Conv2D, Dropout
+from tensorflow.keras.layers import BatchNormalization
 from tensorflow.keras import regularizers
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+
 from constants import *
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 
 
 def process(data):
-    path = pathlib.Path(data)
-    filepaths = list(path.glob(r"*/*.jpg"))
+    path = pathlib.Path(data)  # Conversion de la donnée String en une donnée Path
+    filepaths = list(path.glob(r"*/*.jpg"))  # On itère sur tous les sous-répertoires
     labels = list(map(lambda x: os.path.split(os.path.split(x)[0])[1],
-                      filepaths))
+                      filepaths))  # On sépare le label du filepath et on le stocke
     df1 = pd.Series(filepaths, name='filepaths').astype(str)
     df2 = pd.Series(labels, name='labels')
-    df = pd.concat([df1, df2], axis=1)
+    df = pd.concat([df1, df2], axis=1)  # Création du DataFrame
     return df
 
 
@@ -25,32 +32,34 @@ df_train = process(DIR_TRAIN)
 df_test = process(DIR_TEST)
 df_valid = process(DIR_VALID)
 
-generator = ImageDataGenerator()
-train_ds = generator.flow_from_dataframe(
-    dataframe=df_train,
-    x_col='filepaths',
-    y_col='labels',
-    target_size=(224, 224),
-    batch_size=64,
-    subset='training',
-    random_seed=42)
+image_generator = ImageDataGenerator()
 
-valid_ds = generator.flow_from_dataframe(
-    dataframe=df_valid,
-    x_col='filepaths',
-    y_col='labels',
-    target_size=(224, 224),
-    batch_size=32,
-    subset='training')
+train_ds = image_generator.flow_from_dataframe(dataframe=df_train,
+                                               x_col='filepaths',
+                                               y_col='labels',
+                                               target_size=(224, 224),
+                                               batch_size=64,
+                                               subset='training',
+                                               random_seed=42)
 
-test_ds = generator.flow_from_dataframe(
+test_ds = image_generator.flow_from_dataframe(
     dataframe=df_test,
     x_col='filepaths',
     y_col='labels',
     target_size=(224, 224),
+    batch_size=32
+)
+
+valid_ds = image_generator.flow_from_dataframe(
+    dataframe=df_valid,
+    x_col='filepaths',
+    y_col='labels',
+    subset='training',
+    target_size=(224, 224),
     batch_size=32)
 
 classes = list(train_ds.class_indices.keys())
+print(classes)
 
 model = Sequential()
 
@@ -78,38 +87,14 @@ model.add(Dense(10, activation='softmax'))
 
 model.summary()
 
-filepath = "/content/drive/MyDrive/test_db_100/my_model_100.h5"
+filepath = "local_model.h5"
 checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
 early_stopping_monitor = EarlyStopping(monitor='val_accuracy', patience=3, restore_best_weights=True)
 
 callbacks_list = [checkpoint, early_stopping_monitor]
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=["accuracy"])
 
-model.compile(loss=keras.losses.categorical_crossentropy, optimizer='adam', metrics=["accuracy"])
-
-history = model.fit(train_ds, epochs=30, verbose=1, validation_data=valid_ds, callbacks=[callbacks_list])
-
-acc = history.history['accuracy']
-loss = history.history['loss']
-epochs = range(len(acc))
-
-# Plot: accuracy vs epoch
-plt.plot(epochs, acc, 'r', label='Training accuracy')
-plt.title('Training accuracy')
-plt.xlabel('Epochs')
-plt.ylabel('Accuracy')
-plt.legend(loc=0)
-plt.figure()
-plt.show()
-
-# Plot: loss values vs epoch
-plt.plot(epochs, loss, 'r', label='Training loss')
-plt.title('Training loss')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.legend(loc=0)
-plt.figure()
-plt.show()
-
+history = model.fit(train_ds, epochs=30, verbose=1, validation_data=valid_ds, callbacks=callbacks_list)
 model.evaluate(test_ds, use_multiprocessing=True, workers=10)
 
-model.save('models/local_model.h5')
+model.save('local_model.h5')
