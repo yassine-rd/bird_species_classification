f7d8c43bad5e57f09ba62d87fd6faeca23e3ba6c
Yassine RODANI <76453252+yassine-rd@users.noreply.github.com>
2022-06-20 14:45:08 +0200

    updating constants file

--- flask-main/constants.py
+++ flask-main/constants.py
@@ -1,7 +1,7 @@
 import os
 
 # This will establish paths to our main directories
-DIR_BASE = '../test_db_100'
+DIR_BASE = '../dataset'
 DIR_TRAIN = os.path.join(DIR_BASE, 'train')
 DIR_VALID = os.path.join(DIR_BASE, 'valid')
 DIR_TEST = os.path.join(DIR_BASE, 'test')
